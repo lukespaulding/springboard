eafb84c28fb5cb99a33f1323ca3a7f7eb2c1b5a2
luke_spaulding <lukesp@outlook.com>
2023-02-05 11:19:33 -0500

    I found it interesting that C did not make converting int to string super intuitive. Was an interesting issue to run across

--- two_week/fizzbuzz/fizzbuzz_print.cc
+++ two_week/fizzbuzz/fizzbuzz_print.cc
@@ -1,19 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-////////////////////////////////
-// ADD FIZZBUZZ FUNCTION HERE //
-////////////////////////////////
+string fizzbuzz (int i){
+    //a number divisible by 3 and 5 will always be divisible by 15
+    if (( i % 15) == 0 ){
+        return ("fizzbuzz\t");
+    }
+    //if number is divisible by 3, return 'fizz'
+    else if (( i % 3 ) == 0) {
+            return("fizz\t");                
+    }
+        // number divisible by 5, return 'buzz' 
+        // in place of the number
+    else if (( i % 5 ) == 0){                      
+            return("buzz\t");                
+    }
+    else {
+     // return the number
+        std::string text = "";
+        text += std::to_string(i);
+        return(text);
+    }
 
-int main ()
-{
-  for (int n=1; n<=50; ++n)
-  {
-    /////////////////////////////////////////////
-    // ADD CODE TO CALL FIZZBUZZ FUNCTION HERE //
-    /////////////////////////////////////////////
-
-  }
 }
 
+int main ()
+   {
+     for (int n=1; n<=50; ++n)
+     {
+       cout << fizzbuzz(n);
+     }
+  }
