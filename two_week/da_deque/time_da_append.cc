450003e0ef80065b9c9ec152c109e869e3b65664
VrangrGitHub <lukesp@outlook.com>
2023-02-05 15:46:12 -0500

    prepend

--- two_week/da_deque/time_da_append.cc
+++ two_week/da_deque/time_da_append.cc
@@ -7,6 +7,85 @@
 
 using namespace std;
 
+class DynamicArray {
+private:
+	// Pointer to store array
+	int* array = NULL;
+	// Size of array
+	int size;
+	// Container size
+	int capacity;
+
+public:
+	// Default constructor with size 1
+	DynamicArray()
+	{
+		capacity = 1;
+		size = 0;
+		array = new int[capacity];
+	}
+
+	// user input for size
+	DynamicArray(int capacity)
+	{
+		this->capacity = capacity;
+		array = new int[capacity];
+		size = 0;
+	}
+
+	// Returns array size
+	int getSize() { return size; }
+
+	// Returns container size
+	int getCapacity() { return capacity; }
+
+	// append 
+	void append(int value)
+	{
+		//checks to see if there is room in the array for value
+		if (size == capacity) {
+
+			// if not then grow the array by double
+			growArray();
+		}
+
+		// insert element
+		array[size] = value;
+		// increment the size or last_index+1
+		size++;
+	}
+
+	// Increase the array size by double of current capacity
+	void growArray()
+	{
+
+		// Creates a new array - twice the size
+		int* temp = new int[capacity * 2];
+
+		capacity = capacity * 2;
+		// copies the old array elements into the new
+		for (int i = 0; i < size; i++) {
+			temp[i] = array[i];
+		}
+
+		// Delete old array
+		delete[] array;
+
+		// Assign newly created temp array to original array
+		array = temp;
+	}
+
+	bool isEmpty()
+	{
+		if (size == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
 int main ()
 {
   vector<vector<int32_t>> time_data;
@@ -18,13 +97,14 @@ int main ()
   {
     time_data.push_back(vector<int32_t>(num_elems, 0));
 
-    auto container = std::make_unique<int[]>(num_elems);
+    
+    DynamicArray container(num_elems);
 
     for (int32_t elem_idx=0; elem_idx<num_elems; ++elem_idx)
     {
       auto start = chrono::high_resolution_clock::now();
 
-      container[elem_idx] = elem_idx;
+      container.append(elem_idx);
 
       auto end = chrono::high_resolution_clock::now();
       auto elapsed__ns = chrono::duration_cast<chrono::nanoseconds>(
--- two_week/da_deque/time_da_prepend.cc
+++ two_week/da_deque/time_da_prepend.cc
@@ -10,6 +10,87 @@
 
 using namespace std;
 
+
+
+class DynamicArray {
+private:
+	// Pointer to store array
+	int* array = NULL;
+	// Size of array
+	int size;
+	// Container size
+	int capacity;
+
+public:
+	// Default constructor with size 1
+	DynamicArray()
+	{
+		capacity = 1;
+		size = 0;
+		array = new int[capacity];
+	}
+
+	// user input for size
+	DynamicArray(int capacity)
+	{
+		this->capacity = capacity;
+		array = new int[capacity];
+		size = 0;
+	}
+
+	// Returns array size
+	int getSize() { return size; }
+
+	// Returns container size
+	int getCapacity() { return capacity; }
+
+	// append 
+	void append(int value)
+	{
+		//checks to see if there is room in the array for value
+		if (size == capacity) {
+
+			// if not then grow the array by double
+			growArray();
+		}
+
+		// insert element
+		array[size] = value;
+		// increment the size or last_index+1
+		size++;
+	}
+
+	// Increase the array size by double of current capacity
+	void growArray()
+	{
+
+		// Creates a new array - twice the size
+		int* temp = new int[capacity * 2];
+
+		capacity = capacity * 2;
+		// copies the old array elements into the new
+		for (int i = 0; i < size; i++) {
+			temp[i] = array[i];
+		}
+
+		// Delete old array
+		delete[] array;
+
+		// Assign newly created temp array to original array
+		array = temp;
+	}
+
+	bool isEmpty()
+	{
+		if (size == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
 int main ()
 {
   vector<vector<int32_t>> time_data;
@@ -21,17 +102,13 @@ int main ()
   {
     time_data.push_back(vector<int32_t>(num_elems, 0));
 
-    ////////////////////////////////////////////////////////
-    // CREATE AN EMPTY DYNAMIC ARRAY NAMED container HERE //
-    ////////////////////////////////////////////////////////
+    DynamicArray container(num_elems);
 
     for (int32_t elem_idx=0; elem_idx<num_elems; ++elem_idx)
     {
       auto start = chrono::high_resolution_clock::now();
 
-      ///////////////////////////////////////////////////////
-      // INSERT CODE TO PREPEND elem_idx TO container HERE //
-      ///////////////////////////////////////////////////////
+      
 
       auto end = chrono::high_resolution_clock::now();
       auto elapsed__ns = chrono::duration_cast<chrono::nanoseconds>(
