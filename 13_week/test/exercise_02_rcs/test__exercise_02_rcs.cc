c2f8dd2b07326a742d04cdc3d9a71df69f097a6f
luke_spaulding <lukesp@outlook.com>
2023-04-22 14:11:09 -0400

    exercise #2

--- 13_week/test/exercise_02_rcs/test__exercise_02_rcs.cc
+++ 13_week/test/exercise_02_rcs/test__exercise_02_rcs.cc
@@ -10,7 +10,7 @@ using namespace std;
 void increment_counter (size_t & counter, std::mutex & counter_mutex, size_t num_times)
 {
   for (size_t idx=0; idx<num_times; ++idx) {
-    this_thread::sleep_for(1ms); // simluate a complicated calculation
+    const lock_guard<mutex> lock(counter_mutex);
     counter++;
   }
 }
@@ -20,6 +20,7 @@ TEST_CASE("increment example")
   size_t const num_times   = 1'000;
   size_t       counter     = 0;
   std::mutex   counter_mutex;
+  
 
   std::thread t_1(increment_counter, std::ref(counter), std::ref(counter_mutex), num_times);
   std::thread t_2(increment_counter, std::ref(counter), std::ref(counter_mutex), num_times);
